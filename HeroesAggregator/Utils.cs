4615a9eb76accb7cee9346ad54927cd234464e0c
Dillanm <dillan.mann@gmail.com>
2017-11-27 20:10:09 +0000

    Added versioning Fixed bug where players with space in name couldn't expand their heroes played

---
+++ HeroesAggregator/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+
+namespace HeroesAggregator
+{
+    public static class Utils
+    {
+        public static int GetVersionNumber()
+        {
+            return Convert.ToInt32(System.Text.Encoding.Default.GetString(Properties.Resources.version));
+        }
+    }
+}
